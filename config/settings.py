8ee0c81d6eddaf800c6016ef3ad5ca93b8b7e40f
lucas <lcsgborges@gmail.com>
2025-02-18 23:19:53 -0300

    config: configs para novo app e integrar

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuarios.apps.UsuariosConfig',
+    'notas.apps.NotasConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('usuarios.urls')),
     path('home/', include('usuarios.urls')),
+    path('notas/', include('notas.urls')),
 ]
